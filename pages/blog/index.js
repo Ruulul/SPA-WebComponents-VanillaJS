47d07ccfcf9d4c668bd924a8021ca60df3eddc20
Ruulul <vamjunior01@gmail.com>
2022-10-24 17:28:08 -0300

    rewrite

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,9 +3,7 @@ import css from './index.css' assert { type: "css" };
 
 let { posts } = index;
 for (let post of posts) {
-    post.content = await fetch(
-        post.path.replace('.', 'pages/blog')
-    ).then(r=>r.text());
+    post.content = await fetch('pages/blog/' + post.path).then(r=>r.text());
 }
 export const render = ({path})=>{
     let content;
@@ -14,12 +12,12 @@ export const render = ({path})=>{
         case 'posts':
             content = `
             <ul>Blog posts:
-                ${posts.map(({path, title})=>`<go-to page=${path.replace('.', 'blog')}><li>${title}</li></go-to>`).join('')}
+                ${posts.map(({path, title})=>`<go-to page=${'blog/' + path}><li>${title}</li></go-to>`).join('')}
             </ul>
             `;
             break;
         default:
-            content = posts.find(p=>p.path === './' + path).content;
+            content = posts.find(p=>p.path === path).content;
             content+= `<br><go-to page=blog>Back to posts</go-to>`
     }
     return {
