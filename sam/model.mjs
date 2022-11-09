1b0a17250b2a780ac6edde77777fe200c0719887
Ruulul <vamjunior01@gmail.com>
2022-11-09 17:06:49 -0300

    push_data proposal

--- sam/model.mjs
+++ sam/model.mjs
@@ -3,13 +3,18 @@ import { state } from "./state.mjs"
 export const model = {
     page: 'loading...',
     path: '',
-    propose ({go_to}) {
+    data: {},
+    propose ({go_to, push_data}) {
         if (go_to) {
             if (go_to === this.page + (this.path ? '/' + this.path : '')) return;
             let full_path = go_to.split('/');
             this.page = full_path[0];
             this.path = full_path.slice(1).join('/');
-        } else return;
+        } else 
+        if (push_data) {
+            this.data = {...this.data, ...push_data};
+        } else 
+        return;
 
         this.present(state);
     },
